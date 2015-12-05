444920ca2192c24d42db1554f0dbb6f46cf2e161
D-Feng <deekookee@gmail.com>
2015-12-05 16:37:46 -0500

    median (Kth largest)

--- median.cpp
+++ median.cpp
@@ -1,9 +1,66 @@
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: An integer denotes the middle number of the array.
+     */
+    //     int naive(vector<int> &nums) {
+    //     // write your code here
+    //     int n = nums.size();
+    //     if (n == 0) {
+    //         return 0;
+    //     }
+    //     sort(nums.begin(), nums.end());
+    //     return nums[( n - 1 )/2];
+    // }
+    
+    void swap(vector<int> &nums, int a, int b) {
+    	int temp = nums[a];
+    	nums[a]	= nums[b];
+    	nums[b] = temp;
+    }
+    
+    int findKSmall(vector<int> &nums, int k, int start, int end) {
+
+    	int pivot = end;
+    	int l = start;
+    	int r = end;
+    	while (true) {
+    		while (nums[l] <= nums[pivot] && l < r) {
+    			l++;
+    		}
+    		while (nums[pivot] <= nums[r] && l < r) {
+    			r--;
+    		}
+    		if (r == l) break;
+    		swap(nums, l, r);
+    	}
+
+    	swap(nums, l, pivot);
+
+    	if (l == k) return nums[l];
+
+    	if (l < k) return findKSmall(nums, k, l + 1, end);
+
+    	return findKSmall(nums, k, start, l - 1);
+    }
+
+
+
+
+
     int median(vector<int> &nums) {
-        // write your code here
-        int n = nums.size();
+
+    	int n = nums.size();
         if (n == 0) {
             return 0;
         }
-        sort(nums.begin(), nums.end());
-        return nums[( n - 1 )/2];
+        return findKSmall(nums, ( n - 1 )/2, 0, n - 1);
     }
+
+
+
+
+
+};
+
