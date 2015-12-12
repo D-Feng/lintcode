a37d69aa8748c17ac9a87db3eecc1323bee41f81
D-Feng <deekookee@gmail.com>
2015-12-12 16:58:41 -0500

    happy number version 2

--- happy_number.cpp
+++ happy_number.cpp
@@ -7,35 +7,32 @@ public:
      
      
     const int val_map[10] = {0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
+    
+    int getNextHappy(int n) {
+        int sum = 0;
+        while (n != 0) {
+                
+            int bit = n % 10;
+            n = n / 10;
+            sum += val_map[bit];
+        }
+        return sum;
+    }
+    
+    
     bool isHappy(int n) {
         // Write your code here
         set<int> m_set;
-        m_set.insert(n);
-        int next = 0;
         
-        while (true) {
-            while (n != 0) {
-                
-                int bit = n % 10;
-                n = n / 10;
-                next += val_map[bit];
-            }
-            
-            if (next == 1) {
-                return true;
-            }
-            
-            auto it = m_set.find(next);
-            if (it != m_set.end() ) {
+        while (n != 1) {
+            auto it = m_set.find(n);
+            if (it != m_set.end()) {
                 return false;
-            } else {
-                m_set.insert(next);
             }
-            
-            n = next;
-            next = 0;
+            m_set.insert(n);
+            n = getNextHappy(n);
         }
         
-        return false;
+        return true;
     }
 };
